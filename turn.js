f33245994dfc54ab6cbc17784398ce2beed90732
Dom Christie <christiedom@gmail.com>
2022-06-09 16:41:45 -0700

    Demo export

--- turn.js
+++ turn.js
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-class Turn {
+export default class Turn {
   constructor (action) {
     this.action = action
     this.beforeExitClasses = new Set()
