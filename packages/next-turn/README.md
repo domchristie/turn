cc9432638cd3bca5e38bb61f9e642ff17fbf06fb
Dom Christie <christiedom@gmail.com>
2023-01-26 21:32:16 +0000

    Improve reliability with Turn component lifecycle methods

--- packages/next-turn/README.md
+++ packages/next-turn/README.md
@@ -7,16 +7,14 @@ Animate Next.js page transitions with the View Transitions API.
 npm i --save @domchristie/next-turn
 ```
 
-Import and call `useTurn` in your component, for example in `pages/_app.js`:
+Import and wrap your HTML in the the `Turn` component, for example in `pages/_app.js`:
 
 ```js
-import { useTurn } from '@domchristie/next-turn'
+import Turn from '@domchristie/next-turn'
 
 export default function App ({ Component, pageProps }) {
-  useTurn()
-
   return (
-    <>
+    <Turn>
       <nav>
         <Link href="/" >Home</Link>
         <Link href="/about" >About</Link>
@@ -24,7 +22,7 @@ export default function App ({ Component, pageProps }) {
       <main>
         <Component {...pageProps} />
       </main>
-    </>
+    </Turn>
   )
 }
 
--- packages/next-turn/index.js
+++ packages/next-turn/index.js
@@ -1,42 +1,50 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router.js'
-import { motionSafe } from '../../src/helpers.js'
+import { Component } from 'react'
+import { withRouter } from 'next/router.js'
 import NullTurn from '../../src/null-turn.js'
+import AnimationTurn from './animation-turn.js'
 import ViewTransitionTurn from '../../src/view-transition-turn.js'
 
 let currentTurn = new NullTurn()
 
-export function useTurn () {
-  if (!motionSafe()) return
-
-  const router = useRouter()
-
-  useEffect(() => {
-    currentTurn.snapshot
-      .then(() => currentTurn.enter())
-
-    router.events.on('routeChangeStart', visit)
-    router.events.on('beforeHistoryChange', beforeRender)
-    return () => {
-      router.events.off('routeChangeStart', visit)
-      router.events.off('beforeHistoryChange', beforeRender)
-    }
-  }, [router])
-
-  return () => {
-    currentTurn = new NullTurn()
+class Turn extends Component {
+  componentDidMount () {
+    this.props.router.events.on('routeChangeStart', this.#start)
   }
 
-  function visit () {
+  #start = () => {
     currentTurn.abort()
-    currentTurn = new (document.startViewTransition
-      ? ViewTransitionTurn
-      : NullTurn
-    )()
+    currentTurn = create('advance')
     currentTurn.exit()
   }
 
-  function beforeRender () {
+  shouldComponentUpdate () {
     currentTurn.beforeEnter()
+      .then(() => this.forceUpdate())
+    return false
+  }
+
+  render () {
+    return this.props.children
+  }
+
+  componentDidUpdate () {
+    currentTurn.enter()
+    currentTurn.complete()
+  }
+
+  componentWillUnmount () {
+    currentTurn = new NullTurn()
+    this.props.router.events.off('routeChangeStart', this.#start)
   }
 }
+
+function create (action) {
+  const Klass = useViewTransition() ? ViewTransitionTurn : AnimationTurn
+  return new Klass(action)
+}
+
+function useViewTransition () {
+  return !!document.startViewTransition
+}
+
+export default withRouter(Turn)
